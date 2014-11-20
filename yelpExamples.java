48ad4eda11d29ea868a0507ce6cd8c26c01d8af1
sklezovich <vasioukhina@gmail.com>
2014-11-20 08:08:39 -0800

    Pseudo code
    Using Java in Eclipse

---
+++ yelpExamples.java
@@ -0,0 +1,124 @@
+
+public class yelpExamples {
+	/*{
+  "categories": [
+    [
+      "Indian",
+      "indpak"
+    ],
+    [
+      "Pakistani",
+      "pakistani"
+    ]
+  ],
+  "deals": [
+    {
+      "currency_code": "USD",
+      "image_url": "http://s3-media4.ak.yelpcdn.com/dphoto/ShQGf5qi-52HwPiKyZTZ3w/m.jpg",
+      "options": [
+        {
+          "formatted_original_price": "$20",
+          "formatted_price": "$10",
+          "is_quantity_limited": true,
+          "original_price": 2000,
+          "price": 1000,
+          "purchase_url": "http://www.yelp.com/deal/cC24ccQGIH8mowfu5Vbe0Q/view",
+          "remaining_count": 36,
+          "title": "$10 for $20 voucher"
+        }
+      ],
+      "url": "http://www.yelp.com/biz/urban-curry-san-francisco?deal=1",
+      "is_popular": true,
+      "time_start": 1317414369,
+      "title": "$10 for $20 voucher"
+    }
+  ],
+  "display_phone": "+1-415-677-9744",
+  "gift_certificates": [
+    {
+      "currency_code": "USD",
+      "image_url": "http://s3-media4.ak.yelpcdn.com/bphoto/Hv5vsWpqeaUKepr9nffJnw/m.jpg",
+      "options": [
+        {
+          "formatted_price": "$25",
+          "price": 2500
+        },
+        {
+          "formatted_price": "$50",
+          "price": 5000
+        }
+      ],
+      "url": "http://www.yelp.com/gift-certificates/some-donut-place-pasadena",
+      "id": "ZZy5EwrI3wyHw8y54jZruA",
+      "unused_balances": "CREDIT"
+    }
+  ],
+  "id": "urban-curry-san-francisco",
+  "image_url": "http://s3-media4.ak.yelpcdn.com/bphoto/Hv5vsWpqeaUKepr9nffJnw/ms.jpg",
+  "is_claimed": true,
+  "is_closed": false,
+  "location": {
+    "address": [
+      "523 Broadway"
+    ],
+    "city": "San Francisco",
+    "country_code": "US",
+    "cross_streets": "Kearny St & Romolo Pl",
+    "display_address": [
+      "523 Broadway",
+      "(b/t Kearny St & Romolo Pl)",
+      "North Beach/Telegraph Hill",
+      "San Francisco, CA 94133"
+    ],
+    "neighborhoods": [
+      "North Beach/Telegraph Hill"
+    ],
+    "postal_code": "94133",
+    "state_code": "CA"
+  },
+  "mobile_url": "http://m.yelp.com/biz/EFjaNaqg2foSIM--mML49g",
+  "name": "Urban Curry",
+  "phone": "4156779744",
+  "rating": 4.0,
+  "rating_img_url": "http://media2.ak.yelpcdn.com/static/201012164084228337/img/ico/stars/stars_4.png",
+  "rating_img_url_large": "http://media4.ak.yelpcdn.com/static/20101216169592178/img/ico/stars/stars_large_4.png",
+  "rating_img_url_small": "http://media2.ak.yelpcdn.com/static/20101216418129184/img/ico/stars/stars_small_4.png",
+  "review_count": 227,
+  "reviews": [
+    {
+      "excerpt": "I gave this restaurant two stars just because of the extremely quick delivery and friendly delivery guy, but the food was nothing I would order again....",
+      "id": "-RDZxLTUExM9Q02x4hZmHg",
+      "rating": 2,
+      "rating_image_large_url": "http://media2.ak.yelpcdn.com/static/20101216220207235/img/ico/stars/stars_large_2.png",
+      "rating_image_small_url": "http://media4.ak.yelpcdn.com/static/201012164278297776/img/ico/stars/stars_small_2.png",
+      "rating_image_url": "http://media4.ak.yelpcdn.com/static/201012163489049252/img/ico/stars/stars_2.png",
+      "time_created": 1317939620,
+      "user": {
+        "id": "AUEDVbP9XNlOcgYOAfR8yg",
+        "image_url": "http://s3-media2.ak.yelpcdn.com/photo/0CX0RSoz8NkPlOTo7Ckqdg/ms.jpg",
+        "name": "Holly E."
+      }
+    }
+  ],
+  "snippet_image_url": "http://s3-media1.ak.yelpcdn.com/photo/39c5Vpe0cFCtZ-rmgAr3YQ/ms.jpg",
+  "snippet_text": "Shame on me for taking so long to update a review.\n\nI have been there several times now and love it everytime.\n\nDevour:  My fav the Lamb Tikka Masala, my...",
+  "url": "http://www.yelp.com/biz/urban-curry-san-francisco",
+  "menu_provider": "yelp",
+  "menu_date_updated": 1317414369
+}
+
+https://github.com/Yelp/yelp-api/commit/76b96c7277f6e7bb38b510d8e9deec3154bf2053
+
+var url = 'https://gdata.youtube.com/feeds/api/videos?'
+    + 'q=skateboarding+dog'
+    + '&start-index=21'
+    + '&max-results=10'
+    + '&v=2';
+var response = UrlFetchApp.fetch(url);
+Logger.log(response);
+
+http://rethinkdb.com/docs/external-api-access/
+
+http://www.yelp.com/developers/documentation/v2/iphone
+*/
+}
